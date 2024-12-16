11cf1b45c05f6a0950ddf7e83d96505d2d726216
shepherd-001 <shepherdlegend001@gmail.com>
2024-12-16 14:27:40 +0100

    commit

--- src/main/java/com/shepherd/sheps_project/data/models/User.java
+++ src/main/java/com/shepherd/sheps_project/data/models/User.java
@@ -3,21 +3,17 @@ package com.shepherd.sheps_project.data.models;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
-import lombok.experimental.SuperBuilder;
 
 import java.util.Set;
 
 @Entity
 @Table(name = "users")
-@SuperBuilder
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class User extends BaseModel{
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.UUID)
-//    private String id;
     private String firstName;
     private String lastName;
     @Column(unique = true)
