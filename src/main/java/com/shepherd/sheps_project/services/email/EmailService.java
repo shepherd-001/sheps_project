559243e7593362b96493da9906f2657ad82e7a0a
shepherd-001 <shepherdlegend001@gmail.com>
2024-12-19 11:15:45 +0100

    refactor: application.yaml

--- src/main/java/com/shepherd/sheps_project/services/email/EmailService.java
+++ src/main/java/com/shepherd/sheps_project/services/email/EmailService.java
@@ -1,4 +1,5 @@
 package com.shepherd.sheps_project.services.email;
 
 public interface EmailService {
+    void sendEmail(String to, String subject, String htmlContent);
 }
--- src/main/java/com/shepherd/sheps_project/services/email/EmailServiceImpl.java
+++ src/main/java/com/shepherd/sheps_project/services/email/EmailServiceImpl.java
@@ -1,4 +1,18 @@
 package com.shepherd.sheps_project.services.email;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class EmailServiceImpl implements EmailService{
+//    private final JavaMailSender mailSender;
+
+    @Override
+    public void sendEmail(String to, String subject, String htmlContent) {
+
+    }
 }
